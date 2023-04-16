8da87fec0a76b781e6d8f46badb046db30cc20a8
Bruno Beghelli <bruno.beghelli@gmail.com>
2023-04-16 09:49:35 -0300

    Increases world gravity acceleration

--- src/game.ts
+++ src/game.ts
@@ -13,7 +13,7 @@ const config = {
 	physics: {
           default: 'arcade',
           arcade: {
-              gravity: { y: 600 },
+              gravity: { y: 800 },
               debug: false
           }
       },

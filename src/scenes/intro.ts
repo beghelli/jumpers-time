b5928c4ec82414edf18c6983f4d72e66ebbbdaeb
Bruno Beghelli <bruno.beghelli@gmail.com>
2023-05-03 21:49:10 -0300

    #16 Adds intro music loop

--- src/scenes/intro.ts
+++ src/scenes/intro.ts
@@ -11,10 +11,14 @@ export default class Intro extends Phaser.Scene
     preload()
     {
         this.load.image('logo', 'assets/logo.png');
+		this.load.audio('introMusic', ['assets/intro.mp3']);
     }
 
     create()
     {
+		const music = this.sound.add('introMusic', { loop: true });
+		music.play();
+
     	this.add.image(400, 300, 'logo');
 		new BlinkText(this, this.sys.game.canvas.width / 2, 450, 'Press ENTER to start!', 250);
 
